773db0c7945a2968c1b577eda53cdf69444c6e14
Garett Kinney <garettkinney@gmail.com>
2021-01-18 17:30:43 -0500

    Finish Re-create

--- app/src/main/java/edu/utap/yournavigatordeluxe/MainActivity.kt
+++ app/src/main/java/edu/utap/yournavigatordeluxe/MainActivity.kt
@@ -2,10 +2,29 @@ package edu.utap.yournavigatordeluxe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        // Initialize Bottom Navigation System
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.navigation_recents, R.id.navigation_faves, R.id.navigation_explore, R.id.navigation_gas
+        ))
+        //setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
